836d7fb0acf40c13bf412c301375e8dc5720de85
ee <wewfwfw>
2025-01-18 11:36:30 +0200

    t

---
+++ smartphone.py
@@ -0,0 +1,14 @@
+class Computer:
+    def __init__(self):
+        self.memory = 128
+class Display:
+    def __init__(self):
+        self.resolution = "4k"
+class SmartPhone(Display, Computer):
+    def print_info(self):
+        print(self.resolution)
+        print(self.memory)
+
+
+iphone = SmartPhone()
+iphone.print_info()
